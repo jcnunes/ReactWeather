d5c44645e296121ab90891046b570762d18ebd7c
Joana Pedro <jsofp@outlook.com>
2019-07-10 18:52:46 +0100

    Update project

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -11,8 +11,8 @@ var Weather = React.createClass({
   },
   handleSearch: function (location) {
     var that = this;
-    
-    this.setState({isLoading: true})
+
+    this.setState({isLoading: true});
 
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
@@ -20,23 +20,22 @@ var Weather = React.createClass({
         temp: temp,
         isLoading: false
       });
-
-
     }, function (errorMessage) {
       that.setState({isLoading: false});
-        alert(errorMessage);
+      alert(errorMessage);
     });
   },
   render: function () {
     var {isLoading, temp, location} = this.state;
 
-    function renderMessage() {
+    function renderMessage () {
       if (isLoading) {
-        return <h3>Fetching weather...</h3>
+        return <h3>Fetching weather...</h3>;
       } else if (temp && location) {
-        return <WeatherMessage temp={temp} location={location}/>
+        return <WeatherMessage temp={temp} location={location}/>;
       }
     }
+
     return (
       <div>
         <h3>Weather Component</h3>
